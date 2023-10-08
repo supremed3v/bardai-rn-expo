c14d3a40844696866e430cdb4776c5b782c20ca3
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-10-08 23:25:41 +0500

    added env

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -7,12 +7,10 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBpajk9WTpZ9TbxA68pOZrXnVqAuY_Ml2k",
   authDomain: "bard-ai-6c00d.firebaseapp.com",
   projectId: "bard-ai-6c00d",
   storageBucket: "bard-ai-6c00d.appspot.com",
   messagingSenderId: "63887247155",
-  appId: "1:63887247155:web:ab219251fb14fa36886046",
   measurementId: "G-W6L625SMSJ",
 };
 
